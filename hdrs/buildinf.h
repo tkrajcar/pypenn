dffa18da3a65bbb3cd5761ba5adc25613e43e550
Tim Krajcar <allegro@conmolto.org>
2011-06-10 00:39:00 -0400

    kill some stragglers

--- hdrs/buildinf.h
+++
@@ -1,4 +0,0 @@
-/* This file generated automatically from Makefile */
-#define BUILDDATE "Fri Jun 10 00:00:52 EDT 2011"
-#define COMPILER "gcc -std=gnu99"
-#define CCFLAGS "-ggdb -O -W -Wall -pedantic -Wno-comment -fstack-protector -Wa,--noexecstack -I/usr/include/ -I.. -I../hdrs   -I/usr/include/python2.7 -I/usr/include/mysql  -DBIG_JOINS=1  -fno-strict-aliasing   -DUNIV_LINUX -DUNIV_LINUX  "
